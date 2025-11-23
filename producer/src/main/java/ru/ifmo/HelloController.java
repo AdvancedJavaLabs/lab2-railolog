625578ddcb9af3fe3f399789a924c5077d53d2fa
railolog <railolog@gmail.com>
2025-11-23 19:14:59 +0300

    add swagger

--- producer/src/main/java/ru/ifmo/HelloController.java
+++ producer/src/main/java/ru/ifmo/HelloController.java
@@ -1,5 +1,10 @@
 package ru.ifmo;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.amqp.rabbit.core.RabbitTemplate;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -7,6 +12,7 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@Tag(name = "Producer Controller", description = "API for message production and basic operations")
 public class HelloController {
 
     private final RabbitTemplate rabbitTemplate;
@@ -21,12 +27,22 @@ public class HelloController {
     }
 
     @RequestMapping("/")
+    @Operation(summary = "Get greeting message", description = "Returns a simple greeting message")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Successfully retrieved greeting message")
+    })
     String hello() {
         return "Hello World!";
     }
 
     @RequestMapping("/send")
+    @Operation(summary = "Send message to RabbitMQ", description = "Sends a message to the configured RabbitMQ exchange")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Message sent successfully"),
+            @ApiResponse(responseCode = "400", description = "Invalid message parameter")
+    })
     public void send(
+            @Parameter(description = "Message to be sent to RabbitMQ queue", required = true)
             @RequestParam("message") String message
     ) {
         rabbitTemplate.convertAndSend(exchangeName, "", message);
